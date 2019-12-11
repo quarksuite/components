a3793f9ced0edf9c8ab5fe81f8d2ef2bd96db832
Chatman R. Jr <crjr.code@gmail.com>
2019-12-11 17:21:30 -0500

    Update Storybook settings.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,9 @@
-import { configure } from '@storybook/html';
+import { configure, addParameters } from '@storybook/html';
+
+addParameters({
+  options: {
+    enableShortcuts: false
+  }
+})
 
 configure(require.context('../src', true, /\.stories.js$/), module);
