a86d0fd8a73fa4aca4a073b7eafb5cb740487ab3
Chatman R. Jr <crjr.code@gmail.com>
2020-01-07 20:02:44 -0500

    Update webpack config to use PostCSS

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,10 +3,12 @@ const { resolve } = require('path');
 // Export a function. Accept the base config as only param
 module.exports = async ({ config, mode }) => {
   // Alter the webpack config
-  config.module.rules.push({
-    test: /\.css$/i,
-    use: ['raw-loader', 'postcss-loader']
-  })
+  config.module.rules = [
+    {
+      test: /\.css$/,
+      use: ['raw-loader', 'postcss-loader']
+    }
+  ];
 
   return config;
-}
+};
