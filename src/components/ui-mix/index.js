fe548b6b93c168cdbcf867fd212ea015126fbf92
Chatman R. Jr <crjr.code@gmail.com>
2020-01-09 19:45:35 -0500

    First build of <ui-mix>

--- src/components/ui-mix/index.js
+++ src/components/ui-mix/index.js
@@ -9,14 +9,37 @@ import hostInit from '../shared/host';
 import formStyles from '../shared/form';
 
 export default {
-  base: '#ff0000',
-  blend: '#0000ff',
+  base: 'dodgerblue',
+  blend: 'hotpink',
   amount: 50,
   format: 'hex',
   output: ({ base, blend, amount }) => color.mix(base, blend, amount),
   render: ({ base, blend, amount, format, output }) =>
     html`
       ${hostInit} ${formStyles}
+      <style>
+        :host {
+          --form-flow: row wrap;
+        }
+        form {
+          display: flex;
+          flex-flow: var(--form-flow);
+          justify-content: space-between;
+        }
+
+        .color {
+          margin: var(--field-margins);
+          flex-basis: 49.5%;
+        }
+
+        .range {
+          flex: 1;
+        }
+
+        .range > input {
+          width: 100%;
+        }
+      </style>
       <form action="">
         ${colorKnob('Base Color', base, html.set('base'))}
         ${colorKnob('Blend Target', blend, html.set('blend'))}
