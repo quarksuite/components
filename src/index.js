09f17830772b7fa479f001bdd1527e42e39a206a
Chatman R. Jr <crjr.code@gmail.com>
2020-02-12 18:11:40 -0500

    Fixes build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
 import { define } from 'hybrids';
 
-import * as components from '../src/components';
+import * as components from './components';
 
 define({ ...components });
