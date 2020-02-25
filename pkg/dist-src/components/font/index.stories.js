909c4525c94a74e934528b8e556b76713be2de9d
Chatman R. Jr <crjr.code@gmail.com>
2020-02-25 13:15:27 -0500

    Rebuild package

--- pkg/dist-src/components/font/index.stories.js
+++
@@ -1,7 +0,0 @@
-import { define } from 'hybrids';
-import Font from ".//index.js";
-define('qs-font', Font);
-export default {
-  title: 'Display|Content/<qs-font>'
-};
-export const demo = () => document.createElement('qs-font');
--- pkg/dist-src/components/swatch/index.stories.js
+++
@@ -1,7 +0,0 @@
-import { define } from 'hybrids';
-import Swatch from ".//index.js";
-define('qs-swatch', Swatch);
-export default {
-  title: 'Display|Color/<qs-swatch>'
-};
-export const Demo = () => document.createElement('qs-swatch');
